fa09e205d5142353605fe65817e05e707c98a0c1
SR_projects <84505906+RLprojects-envs@users.noreply.github.com>
2024-05-07 09:12:57 +0100

    Add files via upload

---
+++ RASL_Code/go_LFW.m
@@ -0,0 +1,106 @@
+% Yigang Peng, Arvind Ganesh, November 2009. 
+% Questions? abalasu2@illinois.edu
+%
+% Copyright: Perception and Decision Laboratory, University of Illinois, Urbana-Champaign
+%            Microsoft Research Asia, Beijing
+%
+% Reference: RASL: Robust Alignment by Sparse and Low-rank Decomposition for Linearly Correlated Images  
+%            Yigang Peng, Arvind Ganesh, John Wright, Wenli Xu, and Yi Ma. Proc. of CVPR, 2010.
+%
+
+% robust batch image alignment example
+
+% clear
+clc ;
+clear all;
+close all ;
+
+% addpath
+addpath RASL_toolbox ;
+addpath data ;
+addpath results ;
+
+%% define images' path
+
+currentPath = cd;
+
+% input path
+imagePath = fullfile(currentPath,'data','LFW') ;
+pointPath = fullfile(currentPath,'data','LFW') ; % path to files containing initial feature(eye) coordinates
+dset=dir(strcat(imagePath,'\','0*'));
+for index=1:length(dset)
+mkdir(strcat(imagePath,'\',dset(index).name,'\',dset(index).name,'result-RASL'));
+userName = dset(index).name ;
+% one of 
+% 'Gloria_Macapagal_Arroyo', 'Jennifer_Capriati', 'Laura_Bush',  
+% 'Serena_Williams', 'Barack_Obama', 'Ariel_Sharon', 
+% 'Arnold_Schwarzenegger', 'Colin_Powell', 'Donald_Rumsfeld',
+% 'George_W_Bush', 'Gerhard_Schroeder', 'Hugo_Chavez',
+% 'Jacques_Chirac', 'Jean_Chretien', 'John_Ashcroft',
+% 'Junichiro_Koizumi', 'Lleyton_Hewitt', 'Luiz_Inacio_Lula_da_Silva', 
+% 'Tony_Blair', 'Vladimir_Putin'
+
+% output path
+destRoot = fullfile(currentPath,'results') ;
+destDir = fullfile(destRoot,userName) ;
+if ~exist(destDir,'dir')
+    mkdir(destRoot,userName) ;
+end
+
+%% define parameters
+
+% dispaly flag
+raslpara.DISPLAY = 1 ;
+
+% save flag
+raslpara.saveStart = 1 ;
+raslpara.saveEnd = 1 ;
+raslpara.saveIntermedia = 0 ;
+
+
+% for face images
+raslpara.canonicalImageSize = [ 80 60  ];
+raslpara.canonicalEyeCoords = [ 5  55 ; ...
+                                32 32  ];
+                            
+% parametric tranformation model
+raslpara.transformType = 'AFFINE'; 
+% one of 'TRANSLATION', 'SIMILARITY', 'AFFINE','HOMOGRAPHY'
+
+raslpara.numScales = 1 ; % if numScales > 1, we use multiscales
+
+% main loop
+raslpara.stoppingDelta = .01; % stopping condition of main loop
+raslpara.maxIter = 25; % maximum iteration number of main loops
+
+% inner loop
+raslpara.inner_tol = 1e-6 ;
+raslpara.inner_maxIter = 1000 ;
+raslpara.continuationFlag = 1 ;
+raslpara.mu = 1e-3 ;
+raslpara.lambdac = 1 ; % lambda = lambdac/sqrt(m)
+
+
+%% Get training images
+
+% get initial transformation
+transformationInit = 'SIMILARITY';
+[fileNames, transformations, numImages] = get_training_images( imagePath, pointPath, userName, raslpara.canonicalEyeCoords, transformationInit) ;
+
+
+%% RASL main loop: do robust batch image alignment
+
+[D, Do, A, E, xi, numIterOuter, numIterInner ] = rasl_main(fileNames, transformations, numImages, raslpara, destDir);
+
+%% plot the results
+for c=1:size(A,2);
+    temp=(mat2gray(reshape(A(:,c), raslpara.canonicalImageSize)));
+    imwrite(temp,strcat(imagePath,'\',dset(index).name,'\',dset(index).name,'result-RASL','\',num2str(c),'.jpg'))
+end
+
+meanimage=mat2gray((reshape(sum(A,2), raslpara.canonicalImageSize)));
+imwrite(meanimage,strcat('rasl_prima','\',dset(index).name,'.jpg'));
+%imwrite(meanimage,strcat(imagePath,'\',dset(index).name,'\',dset(index).name,'result-RASL','\',dset(index).name,'mean.jpg'));
+end
+
+
